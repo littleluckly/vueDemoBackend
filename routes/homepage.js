49fdac1faa5003f3f6c6b69d01d02b4cd5667fc7
littleluckly <707798472@qq.com>
2018-09-02 20:27:03 +0800

    点赞和点踩

--- routes/homepage.js
+++ routes/homepage.js
@@ -3,14 +3,14 @@ var router = express.Router();
 var db = require('../utils/db.js')
 
 router.use('/hot', (req, res, next) => {
-	const { pageNo, pageSize } = req.body;
-	db.query(`SELECT * FROM laugh limit ${(pageNo-1)*pageSize}, ${pageSize}`,(result)=>{
+	const { pageNo, pageSize } = req.body; 
+	db.query(`SELECT a.*, SUM(CASE  WHEN b.type = 1 OR b.type = 0 THEN 1 ELSE 0 END) AS allLike, SUM(CASE  WHEN b.type = 1 THEN 1 ELSE 0 END) AS likeCount , SUM(CASE  WHEN b.type = 0 THEN 1 ELSE 0 END) AS dislikeCount FROM laugh a LEFT JOIN laugh_users b ON a.id = b.laugh_id GROUP BY a.id LIMIT ${(pageNo-1)*pageSize}, ${pageSize}`,(result)=>{
 		var resData = {
 			data: result
 		}
 		if(result.length>0){
-			db.query('SELECT count(*) FROM laugh ', (result2) => {
-				res.send({...resData, total: result2[0]['count(*)']})
+			db.query('SELECT count(*) count FROM laugh ', (result2) => {
+				res.send({...resData, total: result2[0]['count']})
 			})
 		}else{
 			res.send({data:[]})
@@ -21,25 +21,20 @@ router.use('/hot', (req, res, next) => {
 router.post('/like', (req,res, next) => {
 	const { laughId, type } = req.body;
 	const { username } = req.cookies;
-	if(type==="like"){
-		// SELECT a.id, a.authorName, COUNT(*) AS num, b.type FROM laugh a LEFT JOIN laugh_users b ON b.laugh_id = a.id WHERE a.id = 2755 AND b.type = 1	
-		db.query(`SELECT * from users WHERE username=${username}`,(result)=>{
-			const userId = result[0].id;
-			db.query(`SELECT * from laugh_users WHERE user_id=${userId} AND laugh_id=${laughId}`,(result2)=>{
+	console.log(laughId, type,username )
+	// if(type=="like"){ 	
+		db.query(`SELECT * from users WHERE userName="${username}"`,(result)=>{
+			const userId = result[0].id; 
+			db.query(`SELECT * from laugh_users WHERE user_id="${userId}" AND laugh_id="${laughId}"`,(result2)=>{
 				if(result2.length===0){
-					db.query(`INSERT INTO laugh_users (laugh_id, user_id, type) VALUES ('${laughId}', '${userId}', '${type=="like"?1:0}')`,(result3)=>{
+					db.query(`INSERT INTO laugh_users (laugh_id, user_id, type) VALUES ("${laughId}", "${userId}", ${type=="like"?1:0})`,(result3)=>{
 						res.send(result3)
 					})
 				}else{
-					res.send({})
+					res.send({err:'您已经点过赞了！'})
 				}
-			})
-		})
-	}else if(type==="dislike"){
-		db.query(`UPDATE laugh SET dislikeCount=dislikeCount+1 WHERE id=${laughId}`, (result)=>{
-			res.send(result);
-		})
-	}
+			})  
+		}) 
 })
 
 module.exports = router;
