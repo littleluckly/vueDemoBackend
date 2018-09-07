6e55f5b7f4bf270679a3b8ac5cdbeb4419b2a627
littleluckly <707798472@qq.com>
2018-09-07 17:54:35 +0800

    评论

--- routes/homepage.js
+++ routes/homepage.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var db = require('../utils/db.js')
 
 router.use('/hot', (req, res, next) => {
-	const { pageNo, pageSize } = req.body; 
+	const { pageNo, pageSize } = req.body;
 	db.query(`SELECT a.*, SUM(CASE  WHEN b.type = 1 OR b.type = 0 THEN 1 ELSE 0 END) AS allLike, SUM(CASE  WHEN b.type = 1 THEN 1 ELSE 0 END) AS likeCount , SUM(CASE  WHEN b.type = 0 THEN 1 ELSE 0 END) AS dislikeCount FROM laugh a LEFT JOIN laugh_users b ON a.id = b.laugh_id GROUP BY a.id LIMIT ${(pageNo-1)*pageSize}, ${pageSize}`,(result)=>{
 		var resData = {
 			data: result
@@ -21,9 +21,9 @@ router.use('/hot', (req, res, next) => {
 // 点赞点踩
 router.post('/like', (req,res, next) => {
 	const { laughId, type } = req.body;
-	const { username } = req.cookies; 
+	const { username } = req.cookies;
 	db.query(`SELECT * from users WHERE userName="${username}"`,(result)=>{
-		const userId = result[0].id; 
+		const userId = result[0].id;
 		db.query(`SELECT * from laugh_users WHERE user_id="${userId}" AND laugh_id="${laughId}"`,(result2)=>{
 			if(result2.length===0){
 				db.query(`INSERT INTO laugh_users (laugh_id, user_id, type) VALUES ("${laughId}", "${userId}", ${type=="like"?1:0})`,(result3)=>{
@@ -32,22 +32,34 @@ router.post('/like', (req,res, next) => {
 			}else{
 				res.send({err:'您已经点过赞了！'})
 			}
-		})  
-	}) 
+		})
+	})
 })
 
-// 评论
+// 发表评论
 router.post('/comment', (req, res, next) => {
 	const { content, laughId } = req.body;
-	const { username } = req.cookies;  
-	console.log(username,'username')
+	const { username } = req.cookies;
 	db.query(`SELECT * from users WHERE userName="${username}"`,(result)=>{
-		const userId = result[0].id;  
+		const userId = result[0].id;
 		console.log(userId,'userId')
 		db.query(`INSERT INTO laugh_user_comment (laugh_id, user_id, content, create_time)
-		VALUES (${laughId}, ${userId}, "${content}", NOW() )`,(result3)=>{ 
+		VALUES (${laughId}, ${userId}, "${content}", NOW() )`,(result3)=>{
 			res.send({status:'ok',msg:''})
 		})
+	})
+})
+
+// 获取评论列表
+router.post('/getCommentList', (req, res, next) => {
+	const { laughId } = req.body;
+	console.log('laughId',laughId)
+	db.query(`select lus.*,u.username  from
+		(SELECT * from laugh_user_comment lu where lu.laugh_id=${laughId}) lus
+		left join  users u
+		on lus.user_id=u.id
+		ORDER BY lus.create_time DESC`,(result3)=>{
+		res.send(result3)
 	})
 })
 
--- spider_jrtt_news.js
+++ spider_jrtt_news.js
@@ -2,7 +2,8 @@
  * 捧腹网首页数据爬虫
  */
 var fs = require('fs');
-var https = require('https');
+// var https = require('https');
+var http = require('http');
 var request = require('request');
 // var request = require('superagent');
 var cheerio = require('cheerio');
@@ -12,33 +13,35 @@ var express = require('express');
 var router = express.Router();
 var db = require('./utils/db.js');
 var i = 6;
- 
-var firstUrl = `https://www.toutiao.com/`;
+
+var firstUrl = `http://kengdie.com/category/lgg/`;
 
 function fetchPage(url){
     firstRequest(url)
 }
 
-function firstRequest(url){  
-    https.get(url, function(res){
-        var html=''; 
+function firstRequest(url){
+    const opt = {
+        cookie: 'UM_distinctid=165a3fca8e4f78-0f0517a9617286-9393265-1fa400-165a3fca8e572d; CNZZDATA3119987=cnzz_eid%3D1342487385-1536053466-https%253A%252F%252Fwww.baidu.com%252F%26ntime%3D1536053466; Hm_lvt_04a2f9e535ccd993cb6a15ea056be0aa=1536054569; Hm_lpvt_04a2f9e535ccd993cb6a15ea056be0aa=1536054569',
+        url: firstUrl,
+        'referer': 'https://www.baidu.com/link?url=iHbX1b5xRnYFW5n-lZdM9eGxZZ-IgM5CbFL0h1YURwPmCx37XGHjoVqUWbPSVIY9&wd=&eqid=8575eb81000195da000000065b8e548f',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+    }
+    http.get(opt, function(res){
+        var html='';
         res.setEncoding('utf-8');
         res.on('data', function(chunk){
             html+=chunk;
         });
         res.on('end',function(){
-            var $ =  cheerio.load(html);   
-            console.log($('.bui-box.single-mode').length)
-            $('.bui-box.single-mode').each( function(idx, item){
-                console.log($(this).find('.title-box a').text())
-                // var src = $(this).find('a').attr('href')
-                // var title = $(this).find('a').text() 
-                // db.query(`INSERT INTO militaryNews (newsTitle, newsSrc) VALUES ("${title}", "${src}")`,(result)=>{
+            var $ =  cheerio.load(html);
+            console.log($('.tag-tna').length)
+            $('.tag-tna').each( function(idx, item){
+                console.log($(this).find('img').attr('src'))
 
-                // })
             })
         })
 
-    }) 
-} 
+    })
+}
 fetchPage(firstUrl)
